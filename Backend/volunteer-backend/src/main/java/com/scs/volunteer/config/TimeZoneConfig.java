c9ad187c8fa623084afab53151089c94e528a65a
Kenny <1371193244@qq.com>
2026-06-09 00:22:23 +0800

    add chat message intention detect

---
+++ Backend/volunteer-backend/src/main/java/com/scs/volunteer/config/TimeZoneConfig.java
@@ -0,0 +1,16 @@
+package com.scs.volunteer.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class TimeZoneConfig {
+    public static final String ZONE_ID = "Asia/Shanghai";
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZONE_ID));
+    }
+}
